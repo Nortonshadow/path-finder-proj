2efee426f7f6e0ca386fa57c2e074adc0fceb57e
Nortonshadow <nortoninvest0r@gmail.com>
2024-11-17 23:07:18 +0800

    Add files via upload

---
+++ D Lite algorithm - Copy.py
@@ -0,0 +1,157 @@
+import math
+import matplotlib.pyplot as plt
+
+show_animation = True
+
+class DStarLitePlanner:
+    def __init__(self, ox, oy, resolution, rr, fc_x, fc_y, tc_x, tc_y, jc_x, jc_y):
+        self.resolution = resolution
+        self.rr = rr
+        self.obstacle_map = None
+        self.x_width, self.y_width = 0, 0
+        self.motion = self.get_motion_model()
+        self.calc_obstacle_map(ox, oy)
+
+        self.fc_x = fc_x
+        self.fc_y = fc_y
+        self.tc_x = tc_x
+        self.tc_y = tc_y
+        self.jc_x = jc_x
+        self.jc_y = jc_y
+
+        self.Delta_C1 = 0.2
+        self.Delta_C2 = 1
+        self.costPerGrid = 1
+
+    class Node:
+        def __init__(self, x, y, cost, parent_index):
+            self.x = x
+            self.y = y
+            self.cost = cost
+            self.parent_index = parent_index
+
+        def __str__(self):
+            return f"{self.x},{self.y},{self.cost},{self.parent_index}"
+
+    def planning(self, sx, sy, gx, gy):
+        start_node = self.Node(self.calc_xy_index(sx, self.min_x),
+                               self.calc_xy_index(sy, self.min_y), 0.0, -1)
+        goal_node = self.Node(self.calc_xy_index(gx, self.min_x),
+                              self.calc_xy_index(gy, self.min_y), 0.0, -1)
+
+        open_set, closed_set = dict(), dict()
+        open_set[self.calc_grid_index(start_node)] = start_node
+
+        while open_set:
+            c_id = min(open_set, key=lambda o: open_set[o].cost + self.calc_heuristic(goal_node, open_set[o]))
+            current = open_set[c_id]
+
+            if current.x == goal_node.x and current.y == goal_node.y:
+                print("Total Trip time required -> ", current.cost)
+                goal_node.parent_index = current.parent_index
+                goal_node.cost = current.cost
+                break
+
+            del open_set[c_id]
+            closed_set[c_id] = current
+
+            for i, _ in enumerate(self.motion):
+                node = self.Node(current.x + self.motion[i][0],
+                                 current.y + self.motion[i][1],
+                                 current.cost + self.motion[i][2] * self.costPerGrid, c_id)
+
+                if self.calc_grid_position(node.x, self.min_x) in self.tc_x and self.calc_grid_position(node.y, self.min_y) in self.tc_y:
+                    node.cost += self.Delta_C1 * self.motion[i][2]
+
+                if self.calc_grid_position(node.x, self.min_x) in self.fc_x and self.calc_grid_position(node.y, self.min_y) in self.fc_y:
+                    node.cost += self.Delta_C2 * self.motion[i][2]
+
+                if self.calc_grid_position(node.x, self.min_x) in self.jc_x and self.calc_grid_position(node.y, self.min_y) in self.jc_y:
+                    node.cost -= (self.Delta_C2) * 0.95 * self.motion[i][2]
+
+                n_id = self.calc_grid_index(node)
+
+                if not self.verify_node(node):
+                    continue
+
+                if n_id in closed_set:
+                    continue
+
+                if n_id not in open_set or open_set[n_id].cost > node.cost:
+                    open_set[n_id] = node
+
+        rx, ry = self.calc_final_path(goal_node, closed_set)
+        return rx, ry
+
+    def calc_final_path(self, goal_node, closed_set):
+        rx, ry = [self.calc_grid_position(goal_node.x, self.min_x)], [
+            self.calc_grid_position(goal_node.y, self.min_y)]
+        parent_index = goal_node.parent_index
+        while parent_index != -1:
+            n = closed_set[parent_index]
+            rx.append(self.calc_grid_position(n.x, self.min_x))
+            ry.append(self.calc_grid_position(n.y, self.min_y))
+            parent_index = n.parent_index
+
+        return rx, ry
+
+    @staticmethod
+    def calc_heuristic(n1, n2):
+        w = 1.0
+        d = w * math.hypot(n1.x - n2.x, n1.y - n2.y)
+        return d
+
+    def calc_grid_position(self, index, min_position):
+        return index * self.resolution + min_position
+
+    def calc_xy_index(self, position, min_pos):
+        return round((position - min_pos) / self.resolution)
+
+    def calc_grid_index(self, node):
+        return (node.y - self.min_y) * self.x_width + (node.x - self.min_x)
+
+    def verify_node(self, node):
+        px = self.calc_grid_position(node.x, self.min_x)
+        py = self.calc_grid_position(node.y, self.min_y)
+
+        if px < self.min_x or py < self.min_y or px >= self.max_x or py >= self.max_y:
+            return False
+
+        if self.obstacle_map[node.x][node.y]:
+            return False
+
+        return True
+
+    def calc_obstacle_map(self, ox, oy):
+        self.min_x = round(min(ox))
+        self.min_y = round(min(oy))
+        self.max_x = round(max(ox))
+        self.max_y = round(max(oy))
+
+        self.x_width = round((self.max_x - self.min_x) / self.resolution)
+        self.y_width = round((self.max_y - self.min_y) / self.resolution)
+
+        self.obstacle_map = [[False for _ in range(self.y_width)]
+                             for _ in range(self.x_width)]
+        for ix in range(self.x_width):
+            x = self.calc_grid_position(ix, self.min_x)
+            for iy in range(self.y_width):
+                y = self.calc_grid_position(iy, self.min_y)
+                for iox, ioy in zip(ox, oy):
+                    d = math.hypot(iox - x, ioy - y)
+                    if d <= self.rr:
+                        self.obstacle_map[ix][iy] = True
+                        break
+
+    @staticmethod
+    def get_motion_model():
+        motion = [[1, 0, 1],
+                  [0, 1, 1],
+                  [-1, 0, 1],
+                  [0, -1, 1],
+                  [-1, -1, math.sqrt(2)],
+                  [-1, 1, math.sqrt(2)],
+                  [1, -1, math.sqrt(2)],
+                  [1, 1, math.sqrt(2)]]
+        return motion
+
